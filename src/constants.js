fc8f8b1d46ecd4cc7c6311b468872cc47e7fa630
Mostafa Aldilati <50502331+mad-lad1@users.noreply.github.com>
2024-02-02 23:26:33 -0500

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const HLS_STREAM = 'https://e1-na7.angelcam.com/cameras/93111/streams/hls/playlist.m3u8?token=eyJjYW1lcmFfaWQiOiI5MzExMSIsImRldmljZV9pZCI6IjkzMTExIiwidGltZSI6MTcwNjkyMTk5MjAwNDAwNSwidGltZW91dCI6MzYwMH0%3D%2E51c4c6580f4afcca22f03696b8b9195a09b5896af3ab60bd3e181b96ae9af997'
+export const HLS_STREAM = 'https://e1-na7.angelcam.com/cameras/93111/streams/hls/playlist.m3u8?token=eyJjYW1lcmFfaWQiOiI5MzExMSIsImRldmljZV9pZCI6IjkzMTExIiwidGltZSI6MTcwNjkzNDI1MTY2NjU3NSwidGltZW91dCI6MzYwMH0%3D%2Efcdcf2380d547718d63ad52ee793daccf6554376a23cdf5688fdca332881037e'
